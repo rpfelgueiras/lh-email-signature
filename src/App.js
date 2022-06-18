a919f067a02d00c9b5fc847196273c56307c56fe
Rui Felgueiras <ruifelgueiras@RuiLaserHubMBP.Home>
2022-06-18 12:54:24 +0100

    Change file input styles

--- src/App.js
+++ src/App.js
@@ -123,7 +123,12 @@ export default function App() {
                   >
                     Photo
                   </label>
-                  <input accept="image/*" type="file" onChange={onImageChange} />
+                  {/* <input accept="image/*" type="file" onChange={onImageChange} /> */}
+                  <input
+                    type="file"
+                    className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+                    onChange={onImageChange}
+                  />
                 </div>
                 {/* {selectedImage && (
                   <div>
