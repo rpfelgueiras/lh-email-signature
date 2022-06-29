cc0d210f6a79de545eea287ff436df1eab9d3a28
Rui Felgueiras <ruifelgueiras@RuiLaserHubMBP.Home>
2022-06-29 20:54:42 +0100

    Copy HTML to the clipboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { MailIcon, ExclamationCircleIcon } from "@heroicons/react/solid";
+
+import React from "react";
+
+import { Transition } from "@headlessui/react";
+import { CheckCircleIcon } from "@heroicons/react/outline";
+import { XIcon } from "@heroicons/react/solid";
 
 export default function App() {
   const [name, setName] = useState("John Doe");
@@ -9,6 +15,10 @@ export default function App() {
   const [email, setEmail] = useState("youremail@laserhub.com");
   const [selectedImageUrl, setSelectedImageUrl] = useState();
   const [imageURLErrorMessage, setimageURLErrorMessage] = useState("");
+  const [showItemCopiedToClipboard, setShowItemCopiedToClipboard] =
+    useState(false);
+
+  const divRef = React.useRef();
 
   function onChangeName(event) {
     setName(event.target.value);
@@ -72,7 +82,9 @@ export default function App() {
   }
 
   function onclickTest(event) {
-    // setEmail(event.target.value);
+    // console.log(divRef.current.innerHTML);
+    navigator.clipboard.writeText(divRef.current.innerHTML);
+    setShowItemCopiedToClipboard(true);
   }
 
   return (
@@ -199,7 +211,10 @@ export default function App() {
                   <div className="dot amber"></div>
                   <div className="dot green"></div>
                 </div>
-                <div className="preview_body border-x border-y border-gray-300 bg-white rounded-b h-80">
+                <div
+                  ref={divRef}
+                  className="preview_body border-x border-y border-gray-300 bg-white rounded-b h-80"
+                >
                   <table
                     cellPadding="0"
                     width="100%"
@@ -417,6 +432,57 @@ export default function App() {
               </div>
             </div>
           </main>
+        </div>
+        <div
+          aria-live="assertive"
+          className="fixed inset-0 flex items-end px-4 py-6 pointer-events-none sm:p-6 sm:items-start"
+        >
+          <div className="w-full flex flex-col items-center space-y-4 sm:items-end">
+            {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+            <Transition
+              show={showItemCopiedToClipboard}
+              as={Fragment}
+              enter="transform ease-out duration-300 transition"
+              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="max-w-sm w-full bg-white shadow-lg rounded-lg pointer-events-auto ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="p-4">
+                  <div className="flex items-start">
+                    <div className="flex-shrink-0">
+                      <CheckCircleIcon
+                        className="h-6 w-6 text-green-400"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="ml-3 w-0 flex-1 pt-0.5">
+                      <p className="text-sm font-medium text-gray-900">
+                        Successfully copied!
+                      </p>
+                      <p className="mt-1 text-sm text-gray-500">
+                        The signature was copied to your clipboard!
+                      </p>
+                    </div>
+                    <div className="ml-4 flex-shrink-0 flex">
+                      <button
+                        type="button"
+                        className="bg-white rounded-md inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        onClick={() => {
+                          setShowItemCopiedToClipboard(false);
+                        }}
+                      >
+                        <span className="sr-only">Close</span>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition>
+          </div>
         </div>
       </div>
     </>
